403a00b78843a5ef5d34e2f10366acb5efb9644f
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2022-12-15 13:40:52 -0500

    started making tests for user model

--- test_user_model.py
+++ test_user_model.py
@@ -8,7 +8,9 @@
 import os
 from unittest import TestCase
 
-from models import db, User, Message, Follows, connect_db
+from models import db, User, Message, Follows, connect_db, DEFAULT_IMAGE_URL, DEFAULT_HEADER_IMAGE_URL
+
+from sqlalchemy.exc import IntegrityError
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -53,3 +55,57 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u1.messages), 0)
         self.assertEqual(len(u1.followers), 0)
+
+    def test_default_images(self):
+        u1 = User.query.get(self.u1_id)
+        
+
+        #User should have the default images for both profile and header
+        self.assertEqual(u1.image_url, DEFAULT_IMAGE_URL)
+        self.assertEqual(u1.header_image_url, DEFAULT_HEADER_IMAGE_URL)
+
+    def test_user_signup_method(self):
+        """ creates a user and tests that the user is created with the correct information """
+        u3 = User.signup("u3", "u3@email.com", "password", None)
+
+        db.session.commit()
+
+        u3_in_db = User.query.get(u3.id)
+
+        self.assertEqual(u3, u3_in_db)
+        self.assertEqual(len(u3_in_db.messages), 0)
+        self.assertEqual(len(u3_in_db.followers), 0)
+        self.assertEqual(len(u3_in_db.following), 0)
+        self.assertEqual(len(u3_in_db.liked_messages), 0)
+
+    def test_user_signup_invalid_username(self):
+        """ creates a user that has a non-unique name and tests for failure """
+
+        u1_repeat_name = User.signup("u1", "not_u1@email.com", "password", None)
+        
+
+        with self.assertRaises(IntegrityError):
+            db.session.commit()
+
+
+
+
+
+#test that an invalid entry responds with an error 
+#error 1 user.signup = use of non-unique name
+
+
+#error 2 = user of none entry in nullable=false
+
+
+
+#test default image
+
+
+#test authenticate 
+
+
+#test is_followed_by
+
+
+#test is_following
